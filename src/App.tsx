fc86f3ffc7771d0de107679d5dc5dd63127d35d1
raditya-rizky <radityarizky@proton.me>
2024-05-01 16:37:34 +0800

    chore: add pizza section

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { ThemeProvider, createTheme } from '@mui/material';
+import { Pizza } from './pizza';
+import { deepOrange } from '@mui/material/colors';
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function RadioButtonsGroup() {
+  const theme = createTheme({
+    palette: {
+      primary: deepOrange
+    },
+    components: {
+      MuiPaper: {
+        styleOverrides: {
+          root: {
+            borderRadius: "12px",
+            // overflow: "hidden",
+            padding: "1rem",
+          },
+        }
+      }
+    },
+  })
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <ThemeProvider theme={theme}>
+      <Pizza />
+    </ThemeProvider>
+  );
 }
-
-export default App
---
+++ src/pizza.tsx
@@ -0,0 +1,94 @@
+import Paper from "@mui/material/Paper";
+import variant from "./assets/pizza.json";
+import { Box, FormControlLabel, Radio, RadioGroup, Typography, styled } from "@mui/material";
+import { useState } from "react";
+
+const Container = styled(Box)(({ theme }) => ({
+  display: "flex",
+  flexDirection: "row",
+  gap: theme.spacing(4),
+  justifyContent: "space-between",
+  [theme.breakpoints.down("md")]: {
+    flexDirection: "column",
+  },
+}))
+
+const PizzaCard = styled(Paper)(({ theme }) => ({
+  flexGrow: 1,
+  transition: "all 0.3s",
+  width: "100%",
+  [theme.breakpoints.down("md")]: {
+    width: "auto",
+  },
+  "&:hover": {
+    boxShadow: theme.shadows[8],
+    cursor: "pointer",
+    transform: "scale(1.05)",
+  },
+}))
+
+export function Pizza() {
+  const [pizzaChosen, setPizzaChosen] = useState("")
+
+  return (
+    <>
+      <RadioGroup>
+        <Typography color={"primary"} fontSize={42} fontWeight={700} variant='h1' mb={3}>
+          Pizza
+        </Typography>
+        <Container>
+          {
+            variant.pizza.map((pizza) => (
+              <PizzaCard
+                elevation={3}
+                key={pizza.name}
+                onClick={() => setPizzaChosen(pizza.name)}
+                sx={{
+                  transform: pizzaChosen === pizza.name ? "scale(1.05)" : "none"
+                }}
+              >
+                <Box
+                  alt={pizza.name}
+                  component={"img"}
+                  src={pizza.image}
+                  sx={{
+                    borderRadius: "8px",
+                    height: 233,
+                    maxHeight: { xs: 233, md: 400 },
+                    objectFit: "cover",
+                    width: "100%",
+                  }}
+                />
+
+                <p
+                  style={{
+                    fontWeight: pizzaChosen === pizza.name ? "bold" : "normal"
+                  }}
+                >
+                  {pizza.name}
+                </p>
+
+                <span
+                  style={{
+                    fontWeight: pizzaChosen === pizza.name ? "bold" : "normal"
+                  }}
+                >${pizza.price}</span>
+
+                <FormControlLabel
+                  control={<Radio />}
+                  label={pizza.name}
+                  value={pizza.name}
+                  sx={{
+                    display: "block",
+                    textAlign: "center",
+                  }}
+                  checked={pizza.name === pizzaChosen}
+                />
+              </PizzaCard>
+            ))
+          }
+        </Container>
+      </RadioGroup>
+    </>
+  )
+}
