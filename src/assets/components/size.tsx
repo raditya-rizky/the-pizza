88b608826ae24daacec657c866f9688092189efc
raditya-rizky <radityarizky@proton.me>
2024-05-02 10:33:15 +0800

    feat: make radio group controllable

--- src/assets/components/size.tsx
+++ src/assets/components/size.tsx
@@ -1,4 +1,4 @@
-import { Box, FormControlLabel, Radio, RadioGroup, Typography } from "@mui/material";
+import { Box, FormControl, FormControlLabel, Radio, RadioGroup, Typography } from "@mui/material";
 import { useContext } from "react";
 import { PizzaContext, PizzaContextType } from "../../App";
 
@@ -11,29 +11,32 @@ export function Size() {
         Size
       </Typography>
 
-      <RadioGroup
-        sx={{
-          display: "flex",
-          flexDirection: "row",
-          gap: "90px",
-        }}
-        defaultValue={size}
-        onChange={(e) => setSize(e.target.value)}
-      >
-        {
-          ["small", "medium", "large"].map((size) => (
-            <FormControlLabel
-              key={size}
-              control={<Radio />}
-              label={size}
-              value={size}
-              sx={{
-                color: "black",
-              }}
-            />
-          ))
-        }
-      </RadioGroup>
+      <FormControl>
+        <RadioGroup
+          sx={{
+            display: "flex",
+            flexDirection: "row",
+            gap: "90px",
+          }}
+          defaultValue={size}
+          value={size}
+          onChange={(e) => setSize(e.target.value)}
+        >
+          {
+            ["small", "medium", "large"].map((size) => (
+              <FormControlLabel
+                key={size}
+                control={<Radio />}
+                label={size}
+                value={size}
+                sx={{
+                  color: "black",
+                }}
+              />
+            ))
+          }
+        </RadioGroup>
+      </FormControl>
     </Box>
   )
 }
